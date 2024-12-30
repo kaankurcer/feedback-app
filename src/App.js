4857a0afa515de6670a076f2189aab7093441ccf
kaankurcer <kkurcer@gmail.com>
2024-12-30 10:09:09 +0300

    feat: add state and conditional styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
       <div className="container">
-        <div>My App</div>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
-  ) 
-} 
+  );
+}
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function Header({ text = 'Feedback UI', bgColor = 'rgba(0,0,0,0.4)', textColor = '#ff6a95' }) {
+function Header({
+  text = "Feedback UI",
+  bgColor = "rgba(0,0,0,0.4)",
+  textColor = "#ff6a95",
+}) {
   const headerStyles = {
-    backgroundColor: bgColor, 
-    color: textColor 
-  }
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
     <header style={headerStyles}>
       <div className="container">
         <h1>{text}</h1>
       </div>
     </header>
-  )
+  );
 }
 
-
-export default Header
+export default Header;
