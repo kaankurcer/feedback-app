54215d5ac027314758555564989fe85426c2bbc0
kaankurcer <kkurcer@gmail.com>
2024-12-30 09:19:00 +0300

    feat: add styling to index.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 function App() {
   return (
     <>
-      <Header text='Hello World' />
+      <Header />
       <div className="container">
         <div>My App</div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-function Header({ text = 'Feedback UI' }) {
+function Header({ text = 'Feedback UI', bgColor = 'rgba(0,0,0,0.4)', textColor = '#ff6a95' }) {
+  const headerStyles = {
+    backgroundColor: bgColor, 
+    color: textColor 
+  }
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h1>{text}</h1>
       </div>
