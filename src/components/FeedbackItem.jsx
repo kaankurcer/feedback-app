864ed48a2869694917d5a6e3716587205f690bcc
kaankurcer <kkurcer@gmail.com>
2024-12-30 10:46:46 +0300

    feat: add delete feedback event and prop drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
+import { FaTimes } from "react-icons/fa";
 import React from "react";
 import Card from "./shared/Card";
-import { useState } from "react";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
